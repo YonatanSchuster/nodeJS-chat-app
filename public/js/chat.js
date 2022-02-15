6bece839a5fa38e8985f93cba186270df0e2c36c
YonatanSchuster <yonatanmsch@gmail.com>
2022-02-15 17:39:54 +0200

    setting up geolocation, use ackowledgment, installing validation module, adding join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,90 @@
-io()
+
+const socket = io()
+
+// Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+// Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
+
+//Options
+const {username, room} = Qs.parse(location.search, {ignoreQueryPrefix: true})
+
+socket.on('message', (message) => {
+    console.log(message)
+    const html = Mustache.render(messageTemplate, {
+        username: message.username,
+        message: message.text,
+        createdAt: moment(message.createdAt).format('hh:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('locationMessage', (message) => {
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate, {
+        username: message.username,
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('roomData', ({room, users}) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room, 
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
+})
+
+$messageForm.addEventListener('submit', (e) => {//document.querySelector -> catching the 'click' by the id and give them things todo
+    e.preventDefault()//prevent the default behavior where the browser go through
+    // disable the coment
+    $messageFormButton.setAttribute('disabled', 'disabled')
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
+
+        if (error) {
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })// sending back massege as event 'increment'to the srvr 
+})
+
+$sendLocationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position) => { //possition is an obj that contain the information we want to share
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('Location shared!')  
+        })
+    })
+})
+
+socket.emit('join' ,{ username, room }, (error) => {
+    if(error){
+        alert(error)
+        location.href = '/' //if there is an error it wiil direct them to the root of the site
+    }
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage } = require('./utils/messages')
+const { generateLocationMessage } = require('./utils/messages')
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -13,8 +17,62 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
+
+io.on('connection', (socket) => { //socket is an obj that contains ifno abut the new connection
     console.log('New webSocket connection! ')
+    
+    socket.on('join', ({username, room}, callback) => {
+        const {error, user } = addUser({ id: socket.id , username, room})
+
+        if(error){
+            return callback(error)
+        }
+
+        socket.join(user.room)
+        //io.to.emit -> allow us to sent a message to everyone in a room without sendig to people on other chat rooms
+        //socket.broadcast.to.emit -> sending an event to everyone except for the specific client, but its limiting it to a specific chat room
+        
+        socket.emit('message', generateMessage('Admin', 'welcome'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has join to the chat! `)) // socket.broadcast.emit is used to send a message to all other users that someone new has joined.
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        })
+
+        callback()
+    })
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id)
+        const filter = new Filter()
+
+        if (filter.isProfane(message)){
+            return callback('Profanity is nat allowed! :( ')
+        }
+
+        io.to(user.room).emit('message', generateMessage(user.username, message)) // use io.emit to emit event the every single connection that's avilable (socket.emit -> emit event to single connection)
+        callback()
+    })
+
+    socket.on('sendLocation', (coords, callback) => {
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)) //the link will get the coardinates of my location and show it at googlemaps
+        callback()
+    })
+    
+    socket.on('disconnect', () => {
+        const user = removeUser(socket.id)
+
+        if(user){
+            io.to(user.room).emit('message', generateMessage('Admin' ,`${user.username} has left!`))
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
+        }
+
+        
+    })
 })
 
 server.listen(port, () => {
